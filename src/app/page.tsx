1b9af54e65a01748337bd2a2ce344d38d7919e1f
momad191 <moemad191@gmail.com>
2025-12-03 16:27:10 +0300

    add momad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-6xl">
-        Heloo
+        Heloo momad ...
       </h1>
       
     </div>
